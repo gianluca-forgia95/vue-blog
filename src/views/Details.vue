bee03dc06087074761218cfa52d4dcd29e15d0c3
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-07-20 10:03:34 +0200

    create form con vue

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -7,15 +7,20 @@
   <div v-if="post" class="post">
       <h3>{{ post.title }}</h3>
       <p class="pre">{{ post.content }}</p>
+  </div>
 
+  <div v-else>
+     <Spinner />
   </div>
 </template>
 
 <script>
 import getPost from '../composables/getPost';
+import Spinner from '../components/Spinner.vue';
 
 export default {
     props: ['id'],
+    components: { Spinner },
     setup(props) {
        const { post, error, load} = getPost(props.id);
 
